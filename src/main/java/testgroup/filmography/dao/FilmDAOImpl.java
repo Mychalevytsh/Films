35ff023d359d642497c1ee27da011a0f0db29a53
Mychalevytsh <mychalevytsh@gmail.com>
2019-09-21 22:49:20 +0300

    FilmDaoCreate

--- src/main/java/testgroup/filmography/dao/FilmDAOImpl.java
+++ src/main/java/testgroup/filmography/dao/FilmDAOImpl.java
@@ -2,14 +2,11 @@ package testgroup.filmography.dao;
 
 import testgroup.filmography.model.Film;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class FilmDAOImpl implements FilmDAO {
-    public static final AtomicInteger AUTO_ID = new AtomicInteger(0);
+    private static final AtomicInteger AUTO_ID = new AtomicInteger(0);
     private static Map<Integer, Film> films = new HashMap<Integer, Film>();
 
     static {
@@ -17,14 +14,15 @@ public class FilmDAOImpl implements FilmDAO {
         film1.setId(AUTO_ID.getAndIncrement());
         film1.setTitle("Inception");
         film1.setYear(2010);
-        film1.setGenre("SCI-FI");
+        film1.setGenre("sci-fi");
         film1.setWatched(true);
-        films.put(film1.getId(),film1);
+        films.put(film1.getId(), film1);
 
+        // + film2, film3, film4, ...
     }
     @Override
     public List<Film> allFilms() {
-        return new ArrayList<>(films.values());
+        return new ArrayList<Film>(films.values());
     }
 
     @Override
